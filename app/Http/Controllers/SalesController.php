9c1389ffaf24a899cd43e6cdf38b99756daf204f
aki843172 <aki843172@gmail.com>
2024-08-01 11:35:01 +0900

    update of step8

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Sale;
 use App\Models\Product;
 
@@ -10,30 +11,46 @@ class SalesController extends Controller
 {
     public function purchase(Request $request)
     {
-        // 入力された商品IDを取得
-        $product_id = $request->input('product_id');
+        // DBトランザクションの開始
+        DB::beginTransaction();
 
-        // 商品情報を取得
-        $product = Product::findOrFail($product_id);
+        try {
+            // 入力された商品IDを取得
+            $product_id = $request->input('product_id');
 
-        // 在庫が0の場合はエラー
-        if ($product->stock <= 0) {
+            // 商品情報を取得
+            $product = Product::findOrFail($product_id);
+
+            // 在庫が0の場合はエラー
+            if ($product->stock <= 0) {
+                return response()->json([
+                    'error' => '在庫がありません'
+                ], 422);
+            }
+
+            // salesテーブルにレコードを追加
+            $sale = new Sale();
+            $sale->product_id = $product_id;
+            $sale->save();
+
+            // productsテーブルの在庫数を減算
+            $product->stock--;
+            $product->save();
+
+            // トランザクションのコミット
+            DB::commit();
+
             return response()->json([
-                'error' => '在庫がありません'
-            ], 422);
+                'message' => '購入完了しました'
+            ], 200);
+        } catch (\Exception $e) {
+            // トランザクションのロールバック
+            DB::rollBack();
+
+            // エラーレスポンスを返す
+            return response()->json([
+                'error' => '購入処理に失敗しました'
+            ], 500);
         }
-
-        // salesテーブルにレコードを追加
-        $sale = new Sale();
-        $sale->product_id = $product_id;
-        $sale->save();
-
-        // productsテーブルの在庫数を減算
-        $product->stock--;
-        $product->save();
-
-        return response()->json([
-            'message' => '購入完了しました'
-        ], 200);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -40,7 +40,7 @@
     </div>
 @endsection
 
-@push('scripts')
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
@@ -81,7 +81,6 @@
         });
     });
 </script>
-@endpush
 
 @push('styles')
 <style>
